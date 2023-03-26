975731d0c8df5634ca4d6e7d8620a9b0d7e096a5
tranducbao1 <19521257@gm.uit.edu.vn>
2023-03-26 12:06:24 +0700

    first

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+const app = express();
+
+app.get("", (req, res) => {
+  res.send("Thanh cong");
+});
+
+app.get("/users", (req, res, next) => {
+  res.json([
+    {
+      id: 1,
+      name: "Giang",
+      address: "QN",
+    },
+    {
+      id: 1,
+      name: "Vu",
+      address: "QN",
+    },
+  ]);
+});
+
+app.listen(3333, "localhost", () => {
+  console.log("connect OK");
+});
